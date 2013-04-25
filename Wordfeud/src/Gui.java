6f7d4d57b9ba0af038e6502dcb341abd5215a4b1
Joost <joostvandenbrandt@hotmail.com>
2013-04-25 20:11:26 +0200

    Eerste classes aangemaakt om de commit te testen.

---
+++ Wordfeud/src/Gui.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class Gui extends JFrame {
+
+	/**
+	 * @param args
+	 * GUI class voor de Wordfeud game
+	 */
+
+	
+}
---
+++ Wordfeud/src/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 * Wordfeud game
+	 * (C) MIN04SOe
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
