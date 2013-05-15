29f0975ec2470fd4585b79746d2fc61bb38158cc
Robin Collard <name24@hotmail.nl>
2013-05-15 21:57:21 +0200

    Account update

--- WordfeudRefactored/src/wordfeud/Account.java
+++ WordfeudRefactored/src/wordfeud/Account.java
@@ -1,5 +1,8 @@
 package wordfeud;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Account {
 
 	private static String accountNaam;
@@ -11,6 +14,32 @@ public class Account {
 	
 	public static String getAccountNaam(){
 		return accountNaam;
+	}
+	
+	public boolean checkModerator(String accountNaam)
+	{
+		Connectie connect = new Connectie();
+		ResultSet myResultSet;
+		
+		myResultSet = connect.voerSelectQueryUit("SELECT rol From Accounts WHERE naam = " + accountNaam + ";");
+		
+		try {
+			while(myResultSet.next())
+			{
+				if(myResultSet.getString("rol").equals("mod")){
+					connect.closeConnection();
+					return true;
+				}
+				else{
+					connect.closeConnection();
+					return false;
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: " + e);
+		}
+		connect.closeConnection();
+		return false;
 	}
 	
 }
