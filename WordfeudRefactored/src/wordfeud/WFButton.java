894d1d581ed1c66672df29c075087792474eb967
Joost <joostvandenbrandt@hotmail.com>
2013-05-16 12:42:12 +0200

    WFButton gemaakt

---
+++ WordfeudRefactored/src/wordfeud/WFButton.java
@@ -0,0 +1,14 @@
+package wordfeud;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class WFButton extends JButton {
+
+	public WFButton(){
+		super();
+		this.setBackground(new Color(44,47,53));
+		this.setForeground(Color.white);
+	}
+}
