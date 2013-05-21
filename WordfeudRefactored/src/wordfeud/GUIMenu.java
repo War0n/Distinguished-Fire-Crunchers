2781dd9eaa1d030f60cbcedca79007ead8040cc7
Robin Collard <name24@hotmail.nl>
2013-05-21 14:32:16 +0200

    typfaal

--- WordfeudRefactored/src/wordfeud/GUIMenu.java
+++ WordfeudRefactored/src/wordfeud/GUIMenu.java
@@ -32,7 +32,7 @@ public class GUIMenu extends JPanel implements ActionListener{
 	private ArrayList<WFButton> myButtons;
 	private GridLayout myGridLayout;
 	private CompetitiesMenu myCompetitiesMenu;
-	private Moderatoe myModPanel;
+	private Moderator myModPanel;
 	private static Account currentAccount;
 	
 	public GUIMenu(){
@@ -102,7 +102,7 @@ public class GUIMenu extends JPanel implements ActionListener{
 			currentAccount = new Account();
 			if(currentAccount.checkModerator(currentAccount.getAccountNaam()))
 			{
-				myModPanel = new Moderatoe();
+				myModPanel = new Moderator();
 				setParentContentPane(myModPanel);
 			}else{
 				message.setText("Je bent geen moderator!");
