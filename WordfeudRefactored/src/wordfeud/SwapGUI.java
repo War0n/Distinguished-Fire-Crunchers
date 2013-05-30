fa42cfc27d7a789f3da1aea9bcf9eb4d03bab8f3
Joost <joostvandenbrandt@hotmail.com>
2013-05-30 13:04:10 +0200

    Trash icon in swapletters

--- WordfeudRefactored/src/wordfeud/SwapGUI.java
+++ WordfeudRefactored/src/wordfeud/SwapGUI.java
@@ -2,17 +2,22 @@ package wordfeud;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetAdapter;
+import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.ArrayList;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SwapGUI extends JFrame implements Observer {
+@SuppressWarnings("serial")
+public class SwapGUI extends JFrame {
 
 	private Letterbak letters;
 	private Spel curSpel;
@@ -20,18 +25,40 @@ public class SwapGUI extends JFrame implements Observer {
 	private WFButton swapCommit;
 	private WFButton swapCancel;
 	private JPanel cp;
+	private JPanel trashPanel;
+	private ArrayList<Tile> swapTiles;
+	private DropTarget trashDroptarget;
 
 	public SwapGUI(Spel spel) {
 		this.letters = spel.getLetterBak();
 		this.curSpel = spel;
 		this.setTitle("Wissel letters in");
+		swapTiles = new ArrayList<Tile>();
+		
 		cp = (JPanel) this.getContentPane();
 		cp.setLayout(new BoxLayout(cp, BoxLayout.X_AXIS));
 		cp.setBackground(new Color(23, 26, 30));
+		
+		trashPanel = new JPanel();
+		trashPanel.setBackground(new Color(23,26,30));
+		trashPanel.setPreferredSize(new Dimension(40,40));
+		trashPanel.add(new JLabel(new ImageIcon("src/images/WFBin.png")));
+		trashDroptarget = new DropTarget(trashPanel,new DropTargetAdapter() {
+			
+			@Override
+			public void drop(DropTargetDropEvent dtde) {
+				
+				swapTiles.add(/*verlepen tiles toevoegen aan de arraylist*/ null);
+			}
+		});
+		
+		trashPanel.setDropTarget(trashDroptarget);
+		
 		lettersView = new LetterbakPanel(this.letters);
 		swapCommit = new WFButton("Swap them!");
 		swapCancel = new WFButton("Cancel");
 		cp.add(lettersView);
+		cp.add(trashPanel);
 		cp.add(Box.createVerticalStrut(10));
 		cp.add(swapCommit);
 		cp.add(Box.createHorizontalStrut(10));
@@ -68,15 +95,10 @@ public class SwapGUI extends JFrame implements Observer {
 		this.dispose();
 	}
 
-	private void commitAction() {
+	public void commitAction() {
 		curSpel.getVerloop().skipTurn();
+		
 		cancelFrame();
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
