c74a759d6ae1a9a3a770c54bcf0d9aa83ba040bb
Jelle van es <jellevanes@hotmail.com>
2013-05-31 11:42:30 +0200

    observer rol bij registreren

--- WordfeudRefactored/src/wordfeud/SpelVerloop.java
+++ WordfeudRefactored/src/wordfeud/SpelVerloop.java
@@ -45,6 +45,7 @@ public class SpelVerloop implements Runnable {
 	public void play() {
 		vindWoord();
 		puntenTeller();
+		System.out.println(puntenTeller());
 	}
 
 	@SuppressWarnings("static-access")
