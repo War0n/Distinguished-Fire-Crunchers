e4dd78bea9e6e46a15ddd0567ab70d41c2f11516
Robin Collard <name24@hotmail.nl>
2013-05-31 20:16:04 +0200

    spelverloop update

--- WordfeudRefactored/src/wordfeud/SpelVerloop.java
+++ WordfeudRefactored/src/wordfeud/SpelVerloop.java
@@ -27,11 +27,21 @@ public class SpelVerloop implements Runnable {
 	}
 
 	public void gamePlay() {
+		while(!spelOver){
 		if(myTurn()){
-			
+			spel.getSpelPanel().getPlayButton().setEnabled(true);
+			spel.getSpelPanel().getShuffleButton().setEnabled(true);
+			spel.getSpelPanel().getSkipButton().setEnabled(true);
+			spel.getSpelPanel().getSwapButton().setEnabled(true);
+			spel.getSpelPanel().getClearButton().setEnabled(true);
 		}
 		else{
-			spel.getSpelPanel();
+			spel.getSpelPanel().getPlayButton().setEnabled(false);
+			spel.getSpelPanel().getShuffleButton().setEnabled(false);
+			spel.getSpelPanel().getSkipButton().setEnabled(false);
+			spel.getSpelPanel().getSwapButton().setEnabled(false);
+			spel.getSpelPanel().getClearButton().setEnabled(false);
+		}
 		}
 	}
 
