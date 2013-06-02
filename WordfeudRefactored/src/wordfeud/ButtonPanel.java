a931555c6efae29deffc02de03b8e24b577d8997
Joost <joostvandenbrandt@hotmail.com>
2013-06-02 17:52:20 +0200

    Buttons resizen

--- WordfeudRefactored/src/wordfeud/ButtonPanel.java
+++ WordfeudRefactored/src/wordfeud/ButtonPanel.java
@@ -15,7 +15,7 @@ public class ButtonPanel extends JPanel
 
 	public ButtonPanel()
 	{
-		setLayout(new GridLayout(4, 1));
+		setLayout(new GridLayout(3, 1));
 		setBackground(new Color(23,26,30));
 	}
 
