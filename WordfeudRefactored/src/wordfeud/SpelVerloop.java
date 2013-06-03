4e79191a9de163deaf649dd3c01996f505bd8e7c
Robin Collard <name24@hotmail.nl>
2013-06-03 15:19:57 +0200

    spelverloop

--- WordfeudRefactored/src/wordfeud/SpelVerloop.java
+++ WordfeudRefactored/src/wordfeud/SpelVerloop.java
@@ -435,7 +435,6 @@ public class SpelVerloop implements Runnable {
 			int myScore = 0;
 			int opScore = 0;
 			JFrame popup = null;
-			String firstAccount;
 			connect2.voerInsertOrUpdateQueryUit("UPDATE spel SET Toestand_type = 'Finished' WHERE ID = " + spel.getSpelId() + ";");
 			myResultSet = connect2.voerSelectQueryUit("SELECT SUM(score) FROM beurt WHERE Account_naam = '" + Account.getAccountNaam() + "' AND Spel_ID = " + spel.getSpelId() + ";");
 			try{
