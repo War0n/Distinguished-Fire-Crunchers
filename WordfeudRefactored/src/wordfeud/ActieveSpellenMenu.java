7f696b19059ba5238e86292fb6341d558524494f
Joost <joostvandenbrandt@hotmail.com>
2013-06-04 13:02:28 +0200

    Fix #5

--- WordfeudRefactored/src/wordfeud/ActieveSpellenMenu.java
+++ WordfeudRefactored/src/wordfeud/ActieveSpellenMenu.java
@@ -27,16 +27,24 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 	private JPanel spellen;
 	private JPanel myTurns;
 	private JPanel notMyTurns;
+	private JPanel myTurnLabelPanel;
+	private JPanel notMyTurnLabelPanel;
+	private JLabel myTurnLabel;
+	private JLabel notMyTurnLabel;
+
 	private boolean observer;
+	private boolean doNotCheckBeurten;
 	private JScrollPane scrollPane;
 	private WFButton inviteButton;
 	private WFButton backButton;
 	private Connectie connect;
+	private Thread checkBeurtThread;
 
 	private HashMap<WFButton, String[]> playBtn = new HashMap<WFButton, String[]>();
 
 	public ActieveSpellenMenu(boolean observer) {
 		this.observer = observer;
+		doNotCheckBeurten = false;
 		setMinimumSize(new Dimension(630, 700));
 		setPreferredSize(getMinimumSize());
 		setBackground(new Color(23, 26, 30));
@@ -60,7 +68,6 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 		functies.setLayout(new FlowLayout());
 		functies.add(backButton);
 		// functies.add(inviteButton);
-
 		add(head);
 		add(functies);
 
@@ -73,13 +80,11 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 		notMyTurns = new JPanel();
 		notMyTurns.setLayout(new BoxLayout(notMyTurns, BoxLayout.Y_AXIS));
 		notMyTurns.setBackground(this.getBackground());
-		spellen.add(myTurns);
-		spellen.add(notMyTurns);
 		
 		//Verdeling in myTurn en notMyTurn
-		JPanel myTurnLabelPanel = new JPanel();
+		myTurnLabelPanel = new JPanel();
 		myTurnLabelPanel.setBackground(new Color(44,47,53));
-		JLabel myTurnLabel = new JLabel("Jouw beurt");
+		myTurnLabel = new JLabel("Jouw beurt");
 		myTurnLabel.setForeground(Color.white);
 		myTurnLabelPanel.setMaximumSize(new Dimension(650,25));
 		myTurnLabelPanel.setPreferredSize(myTurnLabelPanel.getMaximumSize());
@@ -87,9 +92,9 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 		myTurns.add(myTurnLabelPanel);
 		myTurns.add(Box.createVerticalStrut(5));
 		
-		JPanel notMyTurnLabelPanel = new JPanel();
+		notMyTurnLabelPanel = new JPanel();
 		notMyTurnLabelPanel.setBackground(new Color(44,47,53));
-		JLabel notMyTurnLabel = new JLabel("Niet jouw beurt");
+		notMyTurnLabel = new JLabel("Niet jouw beurt");
 		notMyTurnLabel.setForeground(Color.white);
 		notMyTurnLabelPanel.setMaximumSize(new Dimension(650,25));
 		notMyTurnLabelPanel.setPreferredSize(notMyTurnLabelPanel.getMaximumSize());
@@ -97,14 +102,43 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 		notMyTurns.add(notMyTurnLabelPanel);
 		notMyTurns.add(Box.createVerticalStrut(5));
 		
+		spellen.add(myTurns);
+		spellen.add(notMyTurns);
+		
 		scrollPane = new JScrollPane(spellen);
 		scrollPane
 				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		add(scrollPane);
-
+		checkBeurtThread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(!doNotCheckBeurten){
+					try {
+						Thread.sleep(5000);
+						myTurns.removeAll();
+						myTurns.add(myTurnLabelPanel);
+						myTurns.add(Box.createVerticalStrut(5));
+						
+						notMyTurns.removeAll();
+						notMyTurns.add(notMyTurnLabelPanel);
+						notMyTurns.add(Box.createVerticalStrut(5));
+						showSpellen();
+						System.out.println("in Thread");
+						spellen.revalidate();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	
+				}
+				
+			}
+		});
+		checkBeurtThread.start();
 		showSpellen();
 	}
-	
+
 	@SuppressWarnings("static-access")
 	public boolean myTurnActiveSpellen(int spelId) {
 		boolean myTurn = false;
@@ -115,16 +149,15 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 		rs = con.doSelect("SELECT Account_naam, ID FROM beurt WHERE Spel_ID = "
 				+ spelId + " ORDER BY ID DESC LIMIT 1");
 		try {
-			if(rs.next()) {
+			if (rs.next()) {
 				accountNaam = rs.getString("Account_naam");
 				beurt = rs.getInt("ID");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		if (!Account.getAccountNaam().equals(accountNaam))
-		{
-				myTurn = true;
+		if (!Account.getAccountNaam().equals(accountNaam)) {
+			myTurn = true;
 		}
 		con.closeConnection();
 		return myTurn;
@@ -184,22 +217,21 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 				WFButton accept = new WFButton("open spel");
 				accept.addActionListener(this);
 				playBtn.put(accept, challengeSpecs);
-				
+
 				lineBoxControls.add(accept);
 				vertBox.add(lineBoxText);
 				vertBox.add(Box.createVerticalStrut(5));
 				vertBox.add(lineBoxControls);
 				comp.add(vertBox);
-				if( myTurnActiveSpellen(Integer.parseInt(spelID))){
-					comp.setBackground(new Color(122,162,113));
+				if (myTurnActiveSpellen(Integer.parseInt(spelID))) {
+					comp.setBackground(new Color(122, 162, 113));
 					myTurns.add(comp);
 					myTurns.add(Box.createVerticalStrut(5));
 				} else {
-					comp.setBackground(new Color(156,71,26));
+					comp.setBackground(new Color(156, 71, 26));
 					notMyTurns.add(comp);
 					notMyTurns.add(Box.createVerticalStrut(5));
 				}
-				
 
 			}
 		} catch (SQLException e) {
@@ -221,11 +253,13 @@ public class ActieveSpellenMenu extends JPanel implements ActionListener {
 			} else {
 				setParentContentPane(new ObserverGUI(Integer.parseInt(a[0])));
 			}
+			doNotCheckBeurten = true;
 		} else if (arg0.getSource().equals(backButton)) {
+			doNotCheckBeurten = true;
 			setParentContentPane(new GUIMenu());
 		}
 	}
-
+	
 	public void setParentContentPane(JPanel contentPane) {
 		JFrame root = (JFrame) SwingUtilities.getWindowAncestor(this);
 		root.setContentPane(contentPane);
