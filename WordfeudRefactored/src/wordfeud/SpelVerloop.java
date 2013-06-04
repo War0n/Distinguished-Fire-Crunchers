f955fab0be5d5a47ca0961d7bb0c4ee1ecd6466c
Wesley <wesley@nextgensoftware.nl>
2013-06-04 22:21:59 +0200

    fix voor vindWoord

--- WordfeudRefactored/src/wordfeud/SpelVerloop.java
+++ WordfeudRefactored/src/wordfeud/SpelVerloop.java
@@ -97,7 +97,9 @@ public class SpelVerloop implements Runnable {
 					System.out.println(str + " is een woord.");
 				}
 				else
+				{
 					System.out.println(str + " is geen woord!");
+				}
 			}
 			if( numWoorden == woordenGevonden.size())
 			{
@@ -306,37 +308,39 @@ public class SpelVerloop implements Runnable {
 	{
 		HashMap<Point, Stone> myWord = new HashMap<Point, Stone>();
 		Point tmp = new Point(p.x, p.y);
+		Point beginp = new Point(p.x, p.y);
 		if(horizontaal)
 		{
-			boolean hasTwoOrMore = false;
 			while(nextStone(tmp,'l') != null)
 			{
 				tmp.x -= 1;
-				hasTwoOrMore = true;
+				beginp = new Point(tmp.x, tmp.y);
 			}
+			tmp.x -= 1;
 			do
 			{
-				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));
 				tmp.x += 1;
-			} while(nextStone(tmp,'r') != null);
-			if(hasTwoOrMore == true)
 				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));
+				//tmp.x += 1;
+			} while(nextStone(tmp,'r') != null);
+			/*if(tmp.x - beginp.x > 1)
+				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));*/
 		}
 		else
 		{
-			boolean hasTwoOrMore = false;
 			while(nextStone(tmp,'u') != null)
 			{
 				tmp.y -= 1;
-				hasTwoOrMore = true;
+				beginp = new Point(tmp.x, tmp.y);
 			}
+			tmp.y -= 1;
 			do
 			{
-				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));
 				tmp.y += 1;
-			} while(nextStone(tmp,'d') != null);
-			if(hasTwoOrMore == true)
 				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));
+			} while(nextStone(tmp,'d') != null);
+			/*if(tmp.y - beginp.y > 1)
+				myWord.put(new Point(tmp.x, tmp.y), spelBord.getStoneAt(tmp));*/
 		}
 		
 		if(myWord.size() <= 1)
