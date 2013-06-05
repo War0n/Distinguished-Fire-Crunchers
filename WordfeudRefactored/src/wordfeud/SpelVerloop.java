56bc4eccba9ee2192cc9a251b84cbcc72af1efd8
Wesley <wesley@nextgensoftware.nl>
2013-06-05 11:32:14 +0200

    fix voor blanco letter checks

--- WordfeudRefactored/src/wordfeud/SpelVerloop.java
+++ WordfeudRefactored/src/wordfeud/SpelVerloop.java
@@ -79,7 +79,7 @@ public class SpelVerloop implements Runnable {
 				for(Point pt : myList.get(i).keySet())
 				{
 					int curPt = pt.y * 15 + pt.x;
-					letters.add(new letterSortStruct(curPt, myList.get(i).get(pt).getLetter()));
+					letters.add(new letterSortStruct(curPt, myList.get(i).get(pt).isBlancoLetter()?myList.get(i).get(pt).getBlancoLetter() : myList.get(i).get(pt).getLetter()));
 				}
 				Collections.sort(letters, new letterCompare());
 				for(letterSortStruct ss : letters)
